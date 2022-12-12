a2f70501d7d79663fed4d51f57ddcc6331a94b8f
idealful <idealful@gmail.com>
2022-12-12 14:42:01 +0900

    22. 조합 타입

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,16 +1,15 @@
-// const person: { name: string; age: number } = {
-const person = {
-  name: 'Maximilian',
-  age: 30,
-  hobbies: ['Sports', 'Cooking'],
-};
-
-let favoriteActivities: any[];
-favoriteActivities = ['Sports', 1];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-  // hobby.map() // !!! ERROR !!!
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === 'number' && typeof input2 === 'number') {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
 }
+
+const combiledAges = combine(30, 26);
+console.log(combiledAges);
+
+const combiledNames = combine('Max', 'Anna');
+console.log(combiledNames);
