e4aca28e562216c22c1e06a95c152dc5583e7db7
idealful <idealful@gmail.com>
2022-12-12 16:02:55 +0900

    24. 타입 알리어스 / 사용자 정의 타입

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,15 +1,28 @@
-function combine(input1: number | string, input2: number | string) {
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+function combine(input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptor) {
   let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number') {
-    result = input1 + input2;
+  if ((typeof input1 === 'number' && typeof input2 === 'number') || resultConversion === 'as-number') {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
+
   return result;
+
+  // if (resultConversion === 'as-number') {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
 }
 
-const combiledAges = combine(30, 26);
+const combiledAges = combine(30, 26, 'as-number');
 console.log(combiledAges);
 
-const combiledNames = combine('Max', 'Anna');
+const combiledStringAges = combine('30', '26', 'as-number');
+console.log(combiledStringAges);
+
+const combiledNames = combine('Max', 'Anna', 'as-text');
 console.log(combiledNames);
