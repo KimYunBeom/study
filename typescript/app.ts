7831ff5eb3eeeadbf7b32232bc93adc15a7e9500
idealful <idealful@gmail.com>
2022-12-12 14:06:40 +0900

    18. 배열 타입

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,7 +1,16 @@
-// const person: object = {
-const person: { name: string; age: number } = {
+// const person: { name: string; age: number } = {
+const person = {
   name: 'Maximilian',
   age: 30,
+  hobbies: ['Sports', 'Cooking'],
 };
 
+let favoriteActivities: any[];
+favoriteActivities = ['Sports', 1];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // hobby.map() // !!! ERROR !!!
+}
