623b4ae7a042f8746328b67f8fae2385165973a6
idealful <idealful@gmail.com>
2022-12-27 15:46:04 +0900

    31. 마무리

---
+++ typescript/union-aliases.ts
@@ -0,0 +1,28 @@
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+function combine(input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptor) {
+  let result;
+  if ((typeof input1 === 'number' && typeof input2 === 'number') || resultConversion === 'as-number') {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+
+  return result;
+
+  // if (resultConversion === 'as-number') {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
+}
+
+const combiledAges = combine(30, 26, 'as-number');
+console.log(combiledAges);
+
+const combiledStringAges = combine('30', '26', 'as-number');
+console.log(combiledStringAges);
+
+const combiledNames = combine('Max', 'Anna', 'as-text');
+console.log(combiledNames);
