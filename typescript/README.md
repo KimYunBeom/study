74121f1524c5abcdade828b29bf3d736e4e41f3a
idealful <idealful@gmail.com>
2022-12-27 15:50:38 +0900

    34. "시계 모드" 사용하기

--- typescript/README.md
+++ typescript/README.md
@@ -2,6 +2,8 @@
 
 ---
 
+## 초기설정
+
 ```
 $npm i typescript -g
 $tsc app.ts
@@ -12,3 +14,9 @@ $npm init -y
 $npm i lite-server
 $npm start
 ```
+
+---
+
+## 34. "watch 모드" 사용하기
+
+$tsc app.ts --watch
--- typescript/app.js
+++ typescript/app.js
@@ -1,12 +1,3 @@
-var userInput;
-var userName;
-userInput = 5;
-userInput = 'Max';
-if (typeof userInput === 'string') {
-    userName = userInput;
-}
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-var result = generateError('An error occurred!', 500);
-console.log(result);
+var userName = 'Maximilian';
+userName = 3;
+console.log(userName);
