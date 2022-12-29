3762664e38a2acf1c429cf1969c6ebfc63db1569
idealful <idealful@gmail.com>
2022-12-29 15:17:29 +0900

    43. Strict 컴파일

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -1,8 +1,10 @@
-const button = document.querySelector('button')!;
+const button = document.querySelector('button');
+
+function clickHandler(message: string) {
+  console.log('Clicked!' + message);
+}
 
 // a comment
-button.addEventListener('click', () => {
-  console.log('Clicked!');
-});
-
-const map = new Map();
+if (button) {
+  button.addEventListener('click', clickHandler.bind(null, "You're welcome!"));
+}
