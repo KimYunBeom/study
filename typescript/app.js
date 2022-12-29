0a51184a0ada58d6109536d3c0eded2efb327a77
idealful <idealful@gmail.com>
2022-12-29 13:33:30 +0900

    38. TypeScript 핵심 라이브러리 이해하기

--- typescript/app.js
+++ typescript/app.js
@@ -1,5 +1,6 @@
 "use strict";
-let age;
-age = 30;
-const userName = 'Maximilian';
-console.log(userName);
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    console.log('Clicked!');
+});
+const map = new Map();
--- typescript/app.ts
+++ typescript/app.ts
@@ -1,4 +1,7 @@
-let age: number;
-age = 30;
-const userName = 'Maximilian';
-console.log(userName);
+const button = document.querySelector('button')!;
+
+button.addEventListener('click', () => {
+  console.log('Clicked!');
+});
+
+const map = new Map();
