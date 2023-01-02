0768811f554013d50688af14a04c2b5a52a3e93f
idealful <idealful@gmail.com>
2023-01-02 14:29:59 +0900

    66. 속성 및 "보호된" 수정자 재정의

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -1,7 +1,7 @@
 class Department {
   // private readonly id: string;
   // private name: string;
-  private employees: string[] = [];
+  protected employees: string[] = []; // private과 비슷하지만 상속 class에게도 권한을 허용
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
@@ -38,6 +38,13 @@ class AccountingDepartment extends Department {
     super(id, 'Accounting');
   }
 
+  addEmployee(name: string) {
+    if (name === 'Max') {
+      return;
+    }
+    this.employees.push(name);
+  }
+
   addReport(text: string) {
     this.reports.push(text);
   }
@@ -64,7 +71,11 @@ const accounting = new AccountingDepartment('d2', []);
 
 accounting.addReport('Something went wrong...');
 
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+
 accounting.printReports();
+accounting.printEmployeeInformation();
 
 // const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
 
